3c338750098936bd0be5915d4af5ff571eda202b
aadarsh sureshbabu <anishsme@gmail.com>
2022-10-30 15:04:15 +0530

    second commit

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -3,7 +3,29 @@ import "./Login.css";
 function Login(){
     return(<>
     <div className="login-form">
-        
+        <h1 className="label">
+        e-Finder
+        </h1>
+        <h5>an initiative of Goverment of India and Indian Police Service(IPS) </h5>
+        <div className="textboxes">
+            <div className="name">
+                <label htmlFor="name"><h3>Name</h3></label>
+                <input type="text" name="" id="name" />
+            </div>
+            <div className="password">
+            <h3>Password</h3>
+                <input type="password" name="" id="" />
+            </div>
+            <div className="email">
+            <h3>Email-ID/Phone number</h3>
+                <input type="email" name="" id="" />
+            </div>
+            <div className="button-area">
+            <button className="login">LOG IN</button>
+            <button className="sign-up">SIGN UP</button>
+        </div>
+        </div>
+       
     </div>
     </>)
 }
